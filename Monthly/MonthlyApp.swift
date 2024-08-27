8c28996c037e447d5bde8903f1326f0a7844f376
Shadat Rahman <shadat.rahman.464@gmail.com>
2024-08-28 01:52:58 +0600

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Shadat Rahman on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
